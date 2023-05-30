17cedbf4fcb92ae447fc146a1fdbcc131931bc25
TheunissenSil <theunissensil@gmail.com>
2023-05-30 13:41:34 +0200

    Read users

--- Website/src/pages/Beheer.jsx
+++ Website/src/pages/Beheer.jsx
@@ -1,12 +1,35 @@
 import { useEffect, useState } from "react";
 import { NavLink, Outlet, useNavigate } from "react-router-dom";
-import { doc, getDoc } from "firebase/firestore";
+import { collection, query, getDocs } from "firebase/firestore";
 import { db } from '../Firebase-Config';
 
+
 export default function Beheer() {
+
+    let users = [];
+ 
+    const fetchUsers = async () => {
+       
+        const q = query(collection(db, "users"));
+
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        users.push(doc.data());
+        });
+       
+    }
+   
+    useEffect(()=>{
+        fetchUsers();
+    }, [])
+
+    console.log(users); 
+
     return (
         <div className="main_content">
-            <h1 className="col-md-12">Beheer</h1>
+            <h1 className="col-md-12">Medewerkers</h1>
+            
         </div>
     )
 }
