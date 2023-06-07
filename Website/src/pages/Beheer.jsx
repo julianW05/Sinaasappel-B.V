7351268b5503ac0fbd600e239f35df62b770eda7
TheunissenSil <theunissensil@gmail.com>
2023-06-07 14:54:57 +0200

    test

--- Website/src/pages/Beheer.jsx
+++ Website/src/pages/Beheer.jsx
@@ -1,35 +1,73 @@
 import { useEffect, useState } from "react";
 import { NavLink, Outlet, useNavigate } from "react-router-dom";
-import { collection, query, getDocs } from "firebase/firestore";
+import { collection, query, getDocs, updateDoc, deleteDoc, doc } from "firebase/firestore";
 import { db } from '../Firebase-Config';
 
-
 export default function Beheer() {
+    const [users, setUsers] = useState([]);
 
-    let users = [];
- 
     const fetchUsers = async () => {
-       
         const q = query(collection(db, "users"));
-
         const querySnapshot = await getDocs(q);
+        const usersData = [];
         querySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        users.push(doc.data());
+            usersData.push({ id: doc.id, ...doc.data() });
         });
-       
-    }
-   
-    useEffect(()=>{
-        fetchUsers();
-    }, [])
+        setUsers(usersData);
+    };
 
-    console.log(users); 
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
+    const handleSelectChange = async (event, userId) => {
+        const { value } = event.target;
+        const updatedUsers = users.map((user) => {
+            if (user.id === userId) {
+                return { ...user, rol: value };
+            }
+            return user;
+        });
+        setUsers(updatedUsers);
+        const userRef = doc(db, "users", userId);
+        await updateDoc(userRef, { rol: value });
+    };
+
+    const handleDeleteClick = async (userId) => {
+        const confirmDelete = window.confirm("Are you sure you want to delete this user?");
+        if (confirmDelete) {
+            const userRef = doc(db, "users", userId);
+            await deleteDoc(userRef);
+            const updatedUsers = users.filter((user) => user.id !== userId);
+            setUsers(updatedUsers);
+        }
+    };
 
     return (
         <div className="main_content">
             <h1 className="col-md-12">Medewerkers</h1>
-            
+            <table>
+                <tbody>
+                    {users.map((user) => (
+                        <tr key={user.id}>
+                            <td>
+                                <p>{user.name}</p>
+                                <select
+                                    id=""
+                                    value={user.rol}
+                                    onChange={(event) => handleSelectChange(event, user.id)}
+                                >
+                                    <option value="beheerder">Beheerder</option>
+                                    <option value="onderhoudsmedewerker">Onderhoudsmedewerker</option>
+                                    <option value="schoonmaker">Schoonmaker</option>
+                                    <option value="klant">Klant</option>
+                                </select>
+                                <button onClick={() => handleDeleteClick(user.id)}>Delete</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
-    )
+    );
 }
