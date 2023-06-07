8dc0f7275cabbe08f814ef5404c44f8d779e10ce
TheunissenSil <theunissensil@gmail.com>
2023-06-07 23:15:50 +0200

    Update Boekingen.jsx

--- Website/src/pages/Boekingen.jsx
+++ Website/src/pages/Boekingen.jsx
@@ -1,12 +1,48 @@
 import { useEffect, useState } from "react";
-import { NavLink, Outlet, useNavigate } from "react-router-dom";
-import { doc, getDoc } from "firebase/firestore";
-import { db } from '../Firebase-Config';
+import { NavLink, Link, Outlet, useNavigate } from "react-router-dom";
+import { collection, query, getDocs, doc, deleteDoc } from "firebase/firestore";
+import { db } from "../Firebase-Config";
 
 export default function Boekingen() {
-    return (
-        <div className="main_content">
-            <h1 className="col-md-12">Boekingen</h1>
+  let boekingenTemp = [];
+  const [boekingen, setBoekingen] = useState([]);
+
+  const getBoekingen = async () => {
+    const q = query(collection(db, "boekingen"));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      boekingenTemp.push(doc);
+    });
+    setBoekingen(boekingenTemp);
+  };
+
+  const handleDeleteClick = async (boekingenID) => {
+    const confirmDelete = window.confirm(
+      "Weet je zeker dat je deze boeking wilt verwijderen?"
+    );
+    if (confirmDelete) {
+      const userRef = doc(db, "boekingen", boekingenID);
+      await deleteDoc(userRef);
+      const updatedBoekingen = boekingen.filter(
+        (boeking) => boeking.id !== boekingenID
+      );
+      setBoekingen(updatedBoekingen);
+    }
+  };
+
+  useEffect(() => {
+    getBoekingen();
+  }, []);
+
+  return (
+    <div className="main_back">
+      <div className="blur">
+        <div className="main_box row">
+          <h1 className="col-md-12">Boekingen</h1>
+          <div className="klachten_list col-md-12"></div>
+          <Outlet />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
