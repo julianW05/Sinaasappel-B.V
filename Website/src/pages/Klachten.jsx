32909423a39bfbb7dd0c439938f898d59fe5d3a6
JulianW051 <julianwessels1@gmail.com>
2023-06-14 09:34:43 +0200

    klachten toewijzen half af

--- Website/src/pages/Klachten.jsx
+++ Website/src/pages/Klachten.jsx
@@ -1,22 +1,45 @@
 import { useEffect, useState } from "react";
 import { NavLink, Link, Outlet, useNavigate, useParams } from "react-router-dom";
-import { collection, query, getDocs, doc, deleteDoc } from "firebase/firestore";
+import { collection, query, getDocs, doc, deleteDoc, updateDoc } from "firebase/firestore";
 import { db } from '../Firebase-Config';
 
 export default function Klachten() {
     const { userID } = useParams();
+    const [users, setUsers] = useState([]);
     let klachtenTemp = [];
     const [klachten, setKlachten] = useState([]);
 
-    const getKlachten = async() => {
-        const q = query(collection(db, "klachten"));
+    const fetchUsers = async () => {
+        const q = query(collection(db, "users"));
+        const querySnapshot = await getDocs(q);
+        const usersData = [];
+        querySnapshot.forEach((doc) => {
+          usersData.push({ id: doc.id, ...doc.data() });
+        });
+        setUsers(usersData);
+      };
 
+      const getKlachten = async() => {
+        const q = query(collection(db, "klachten"));
         const querySnapshot = await getDocs(q);
         querySnapshot.forEach((doc) => {
-        klachtenTemp.push(doc);
+            klachtenTemp.push({ id: doc.id, ...doc.data() });
         });
         setKlachten(klachtenTemp);
     }
+
+    const handleSelectChange = async (event, klachtId) => {
+        const { value } = event.target;
+        const updatedKlachten = klachten.map((klacht) => {
+          if (klacht.id === klachtId) {
+            return { ...klacht, toewijzing: value };
+          }
+          return klacht;
+        });
+        setKlachten(updatedKlachten);
+        const klachtRef = doc(db, "klachten", klachtId);
+        await updateDoc(klachtRef, { toewijzing: value });
+      };
 
     const handleDeleteClick = async (klachtID) => {
         const confirmDelete = window.confirm(
@@ -31,6 +54,7 @@ export default function Klachten() {
       };
 
     useEffect(() => {
+        fetchUsers();
         getKlachten();
     }, [])
 
@@ -45,10 +69,17 @@ export default function Klachten() {
                             <tbody>
                                 {klachten.map((klacht) => 
                                     <tr className="klacht" key={klacht.id}>
-                                        <td><strong>{klacht._document.data.value.mapValue.fields.titel.stringValue}</strong></td>
+                                        <td><strong>{klacht.titel}</strong></td>
                                         <td><Link to={`details/${klacht.id}`}>Details</Link></td>
                                         <td><a onClick={() => handleDeleteClick(klacht.id)}>Verwijderen</a></td>
-                                        <td><a>Toewijzen</a></td>
+                                        <td>
+                                            <select id="" value={klacht.toewijzing} onChange={(event) => handleSelectChange(event, klacht.id)}>
+                                                <option value="">Niemand</option>
+                                                <option value="beheerder">Beheerder</option>
+                                                <option value="onderhoudsmedewerker">Onderhoudsmedewerker</option>
+                                                <option value="schoonmaker">Schoonmaker</option>
+                                            </select>
+                                        </td>
                                     </tr>
                                 )}
                             </tbody>
